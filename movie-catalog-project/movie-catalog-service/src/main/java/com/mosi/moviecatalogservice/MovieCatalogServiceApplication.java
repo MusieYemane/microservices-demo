2754d1aba070ab17232a488c867b920f1a49f52e
MusieYemane <m80007278@gmail.com>
2022-02-23 21:58:21 -0600

    Added Fault tolerance using Hystrix

--- movie-catalog-project/movie-catalog-service/src/main/java/com/mosi/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-project/movie-catalog-service/src/main/java/com/mosi/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,21 +2,27 @@ package com.mosi.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class MovieCatalogServiceApplication {
 
 	//Bean creates a singleton instance that wil be used throughout the application
 	@Bean
 	@LoadBalanced  // tells rest template to not expect the url of other service(hard coded), instead it expects hint and will ask eureka for the exact url/address
 	public RestTemplate restTemplate(){
-		return new RestTemplate();
+		// lets create timeout: so that slow services will not slow the whole infrastructure by taking too many threads, they will be cleared out if they take longer time to respond
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory= new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(3000);
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 	@Bean
 	public WebClient.Builder getWebClientBuilder(){
--- movie-catalog-project/movie-catalog-service/src/main/java/com/mosi/moviecatalogservice/controller/MovieCatalogController.java
+++ movie-catalog-project/movie-catalog-service/src/main/java/com/mosi/moviecatalogservice/controller/MovieCatalogController.java
@@ -1,10 +1,12 @@
 package com.mosi.moviecatalogservice.controller;
 
 
-import com.mosi.moviecatalogservice.domain.CatalogItem;
 import com.mosi.moviecatalogservice.domain.Movie;
 import com.mosi.moviecatalogservice.domain.Rating;
 import com.mosi.moviecatalogservice.domain.Ratings;
+import com.mosi.moviecatalogservice.domain.CatalogItem;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,10 +31,14 @@ public class MovieCatalogController {
 //    @Autowired
 //    private WebClient.Builder webClientBuilder;
 
-    //lets use restTemplate to get movie info from movie-info-service and its rating from rating service
+    //lets use restTemplate to get m ovie info from movie-info-service and its rating from rating service
     //RestTemplates helps us to request rest api from another application,
     // it populates the payload from the api to the class we provide as an argument
      @GetMapping("/{userId}")
+     //Since we used 2 other microservices, we need to create a circuit breaker here(stops sending requests to very slow, or crashed microservices)
+    // we will use hystrix of netflix, which is depreciated.
+     //fall back method sends default custom data(from getFallbackCatalog method)  to client if the requested microservice is not responding
+     @HystrixCommand(fallbackMethod="getFallbackCatalog")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
          // coz our restTemplate is loadBalancer, we do not need exact url of remote server, so we change localhost:8081 to just name of the service
@@ -49,5 +55,11 @@ public class MovieCatalogController {
 //                    .block(); // Change it to synchronous from asynchronous
             return new CatalogItem(movie.getName(), "Desc", rating.getRating());
         }).collect(Collectors.toList());
+    }
+
+    // this is default response to client if any microservices is down or too slow to respond
+    // try this by stoping movie-info-service microservice, and request catalog api from browser)
+    public List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId){
+         return Arrays.asList(new CatalogItem("Default Movie Title", "Fallback response", 0));
     }
 }
